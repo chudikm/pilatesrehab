078230db8997392a145c6783256a113e932841ce
mchudik <mchudik@amazon.com>
2018-02-19 00:01:08 +0000

    upravy na vizuali + zaciatky galerie

--- res/js/custom-script.js
+++ res/js/custom-script.js
@@ -1,9 +1,13 @@
 $( document ).ready(function() {
     $(".card").hover(function(){
       $(this).find("a").addClass("active");
+      $(this).find(".card-body").addClass("active");
+      $(this).find(".card-text").addClass("active");
      },
                   function(){
       $(this).find("a").removeClass("active");
+      $(this).find(".card-body").removeClass("active");
+      $(this).find(".card-text").removeClass("active");
     }
    );
 });    

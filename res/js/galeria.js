84f563538daa3ed14c8877f6ab35d41caca8fe56
mchudik <mchudik@amazon.com>
2018-02-25 23:28:45 +0000

    prva verzia galerie, nacitava thumbnails z api gateway

--- res/js/galeria.js
+++ res/js/galeria.js
@@ -1,40 +1,31 @@
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+$( document ).ready(function() {
+    var dataToPost = {
+        "payload" : {
+            "sortBy" : "date"
+        }
+    }
+    $.ajax({
+        type: 'POST',
+        url: 'https://4e392y3tu3.execute-api.eu-west-1.amazonaws.com/dev/PR_readTags',
+        crossDomain: true,
+        data: JSON.stringify(dataToPost),
+        dataType: 'json',
+        success: function( data, status, jqXHR){
+            console.log(data);
+            processResponse(data);
+        },
+        error: function (responseData, textStatus, errorThrown) {
+            console.log('POST failed.');
+        }
+    });
+        
+    function processResponse(data){
+        console.log("Count:" + data.Count);
+        console.log("Items:" + data.Items[0].Title);
+        
+        for(i=0;i<data.Count; i++){
+            $("#image"+(i+1)).css('background-image', 'url(' + "./galeria/thumb/"+data.Items[i].ImageName + ')');
+        }
+    }
+    
+}); 

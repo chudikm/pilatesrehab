80e49091c8f5f34397532cc63e2a0bfe205ba02e
mchudik <mchudik@amazon.com>
2018-02-28 23:26:13 +0000

    galeria: add-lightbox

--- res/js/galeria.js
+++ res/js/galeria.js
@@ -17,15 +17,53 @@ $( document ).ready(function() {
         error: function (responseData, textStatus, errorThrown) {
             console.log('POST failed.');
         }
+    
     });
         
     function processResponse(data){
         console.log("Count:" + data.Count);
         console.log("Items:" + data.Items[0].Title);
         
+        var divRow;
+        var divObrazkyGaleria = document.getElementById("obrazky_galeria");
         for(i=0;i<data.Count; i++){
-            $("#image"+(i+1)).css('background-image', 'url(' + "./galeria/thumb/"+data.Items[i].ImageName + ')');
+            if(i % 3 == 0){
+                divRow = document.createElement("div");
+                divObrazkyGaleria.appendChild(divRow)
+                divRow.className = "row";
+            }
+            
+            var aElem = document.createElement("a");
+            divRow.appendChild(aElem);
+            aElem.id = "image"+(i+1);
+            
+            aElem.className = "col-sm-4";
+            aElem.href = "./galeria/full/"+data.Items[i].ImageName;
+            aElem.setAttribute("data-toggle","lightbox");
+            aElem.setAttribute("data-gallery","pilatesRehab");
+            
+            var imgElem =  document.createElement("img");
+            aElem.appendChild(imgElem);
+            imgElem.src = "./galeria/thumb/"+data.Items[i].ImageName;
+            imgElem.className = "img-fluid";
+            
+           // $("#image"+(i+1)+" img").attr("src","./galeria/thumb/"+data.Items[i].ImageName);
+          //  $("#image"+(i+1)).attr("href","./galeria/full/"+data.Items[i].ImageName);
+
         }
     }
     
 }); 
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox({
+                    alwaysShowClose: true,
+                    onShown: function() {
+                        console.log('Checking our the events huh?');
+                    },
+                    onNavigate: function(direction, itemIndex){
+                        console.log('Navigating '+direction+'. Current item: '+itemIndex);
+                    }
+                });
+});
